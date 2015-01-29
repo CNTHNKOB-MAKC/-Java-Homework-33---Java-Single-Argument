8d3ddbaf395d52d2c63fb52403a2d9b2f34deacb
Maxim <atomicid@outlook.com>
2015-01-28 21:34:23 -0800

    initial commit of the homework 33. some of the problem ocurred were passing the version with the -D command from command line from the wrong directory< must change the version from the POM.XML dierectory.  WHen you would like to run the fat jar must use target directory

--- src/main/java/core/Phuckidation/SeleniumSingleTitle.java
+++ src/main/java/core/Phuckidation/SeleniumSingleTitle.java
@@ -7,28 +7,37 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class SeleniumSingleTitle {
-	public static void main(String[] args) {
+	public static void main(String[] argsPHAKIN) {
 		 
 		WebDriver driver = new FirefoxDriver();    // Version 1.1 :: Firefox
  
 		String text_case_id = "TC-001.01Selenium";
-		String url = "http://www.newsli.ru/";
-		String title_expected = "Newsli - новости из первых уст";
+		// String url = "http://www.newsli.ru/";
+		 //String title_expected = "Newsli - новости из первых уст";
+		
+
+		
+	 String[] param = argsPHAKIN[0].split("\\|");//to use command line argument [single]
+
+	 	String url = param [0];
+ 	String titleExpected = param[1];
+		
+ 		
 		
 		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		String title_actual = driver.getTitle();
  
-		if (title_expected.equals(title_actual)) {
+		if (titleExpected.equals(title_actual)) {
 			System.out.println("Test Case ID: \t\t" + text_case_id);
 			System.out.println("URL: \t\t\t" + url);
-			System.out.println("Title Expected: \t" + title_expected);
+			System.out.println("Title Expected: \t" + titleExpected);
 			System.out.println("Title Actual: \t\t" + title_actual);
 			System.out.println("Test Case Result: \t" + "PASSED");
 		} else {
 			System.out.println("Test Case ID: \t\t" + text_case_id);
 			System.out.println("URL: \t\t\t" + url);
-			System.out.println("Title Expected: \t" + title_expected);
+			System.out.println("Title Expected: \t" + titleExpected);
 			System.out.println("Title Actual: \t\t" + title_actual);
 			System.out.println("Test Case Result: \t" + "FAILED");
 		}
